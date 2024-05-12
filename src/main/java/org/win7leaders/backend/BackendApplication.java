02d15200ffbf8f133ab94389db5db3ed4656d576
AzmiZakaria <zakariaazmi45@gmail.com>
2024-05-12 19:04:14 +0100

    first commit

---
+++ src/main/java/org/win7leaders/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package org.win7leaders.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
